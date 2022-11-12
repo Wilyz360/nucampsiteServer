5d167886f3090347ef8a007b1f53cd4cabb7c985
ElmoMax <williamzhicay32@gmail.com>
2022-11-12 16:47:03 -0500

    Promotion and Pertner mongoDB & mongoose

--- routes/partnersRouter.js
+++ routes/partnersRouter.js
@@ -1,49 +1,78 @@
 const express = require('express');
+const Partner = require('../models/partners');
+
 const partnerRouter = express.Router();
 
-partnerRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+partnerRouter.route('/') 
+.get((req, res, next) => { // next for deal with errors
+    // get method: the client is asking to send back data for all campsites
+    Partner.find() // this will query the database for all the documents
+    .then( partners => { // acess the results from the find method as campsites
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partners);
+    }).catch( err => next(err) ); // it pass the error to the overall error handler for this express application
 })
-.get((req, res) => {
-    res.end('Will send all the partners to you');
-})
-.post((req, res)=> {
-    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`)
-
+.post((req, res, next) => {
+    // Create new campsite document using the request body which should contain information for the campsite to post from the client
+    Partner.create(req.body).then( partner => { // campsite contain information that was posted
+        console.log('Partner Created', partner); // get information log to the console
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner); // send info about the posted docuemnt to the client
+    }).catch( err => next(err) );
 })
 .put((req, res) => {
     res.statusCode = 403,
-    res.end('PUT operation not supported on /partners');
+    res.end('PUT operation not supported on /partner');
 })
-.delete((req, res) => {
-    res.end('Deleting all partners');
+.delete((req, res, next) => {
+    // delete all the campsites
+    Partner.deleteMany() // delete every documents in the campsite's collection
+    .then( response => { // it gives information about how many documents were deleted
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    }).catch( err => next(err) );
 });
 
 partnerRouter.route('/:partnerId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end(`Will send details of the partner: ${req.params.partnerId} to you`);
+.get((req, res, next) => {
+     // Get docuement by id
+     Partner.findById(req.params.partnerId)
+     .then( partner => { // campsite var will contain the document
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+     })
+     .catch( err => next(err) );
 })
 .post((req, res) => {
     res.statusCode = 403;
-    res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
-
+    res.end(`POST operation not supported on /campstes/${req.params.partnerId}`);
 })
-.put((req, res) => {
-    res.write(`Updating the partner: ${req.params.partnerId}\n`);
-    res.end(`Will update the partner: ${req.body.name}
-        with description: ${req.body.description}`);
+.put((req, res, next) => { 
+    // Update campsite by id
+    Partner.findByIdAndUpdate(req.params.partnerId, {
+        $set: req.body // mongo operator for updating
+    }, {
+        new: true // get update rn
+    })
+    .then( partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch( err => next(err)); 
 })
-.delete((req, res) => {
-    res.end(`Deleting partner: ${req.params.partnerId}`);
-
+.delete((req, res, next) => {
+    Partner.findByIdAndDelete(req.params.partnerId)
+    .then( response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch( err => next(err)); 
 });
 
 module.exports = partnerRouter;
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,49 +1,78 @@
 const express = require('express');
+const Promotion = require('../models/promotion');
+
 const promotionRouter = express.Router();
 
-promotionRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+promotionRouter.route('/') 
+.get((req, res, next) => { // next for deal with errors
+    // get method: the client is asking to send back data for all campsites
+    Promotion.find() // this will query the database for all the documents
+    .then( promotion => { // acess the results from the find method as campsites
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    }).catch( err => next(err) ); // it pass the error to the overall error handler for this express application
 })
-.get((req, res) => {
-    res.end('Will send all the promotions to you');
-})
-.post((req, res)=> {
-    res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`)
-
+.post((req, res, next) => {
+    // Create new campsite document using the request body which should contain information for the campsite to post from the client
+    Promotion.create(req.body).then( promotion => { // campsite contain information that was posted
+        console.log('Promotion Created', promotion); // get information log to the console
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion); // send info about the posted docuemnt to the client
+    }).catch( err => next(err) );
 })
 .put((req, res) => {
     res.statusCode = 403,
-    res.end('PUT operation not supported on /promotions');
+    res.end('PUT operation not supported on /promotion');
 })
-.delete((req, res) => {
-    res.end('Deleting all promotions');
+.delete((req, res, next) => {
+    // delete all the campsites
+    Promotion.deleteMany() // delete every documents in the campsite's collection
+    .then( response => { // it gives information about how many documents were deleted
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    }).catch( err => next(err) );
 });
 
 promotionRouter.route('/:promotionId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end(`Will send details of the promotion: ${req.params.promotionId} to you`);
+.get((req, res, next) => {
+     // Get docuement by id
+     Promotion.findById(req.params.promotionId)
+     .then( promotion => { // campsite var will contain the document
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+     })
+     .catch( err => next(err) );
 })
 .post((req, res) => {
     res.statusCode = 403;
-    res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
-
+    res.end(`POST operation not supported on /promotion/${req.params.promotionId}`);
 })
-.put((req, res) => {
-    res.write(`Updating the promotion: ${req.params.promotionId}\n`);
-    res.end(`Will update the promotion: ${req.body.name}
-        with description: ${req.body.description}`);
+.put((req, res, next) => { 
+    // Update campsite by id
+    Promotion.findByIdAndUpdate(req.params.promotionId, {
+        $set: req.body // mongo operator for updating
+    }, {
+        new: true // get update rn
+    })
+    .then( promotion => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    })
+    .catch( err => next(err)); 
 })
-.delete((req, res) => {
-    res.end(`Deleting promotion: ${req.params.promotionId}`);
-
+.delete((req, res, next) => {
+    Promotion.findByIdAndDelete(req.params.promotionId)
+    .then( response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch( err => next(err)); 
 });
 
 module.exports = promotionRouter;
